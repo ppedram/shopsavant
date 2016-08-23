f5a726822365e67df92f69e36846b752ee48a86d
Julian Wilson <julianrwilson@gmail.com>
2016-08-23 14:15:54 -0700

    Limited visible product results to 100 and added a view on store/shopify link to listing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,11 @@ class ProductsController < ApplicationController
   def index
     collection = Collection.find_by(:handle => params[:collection])
     if params[:order_by] == "sales"
-      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(total_sales: :desc)
+      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(total_sales: :desc).limit(100)
     elsif params[:order_by] == "inventory"
-        @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(total_inventory: :desc)
+        @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(total_inventory: :desc).limit(100)
     else
-      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(product_updated_at: :desc)
+      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).order(product_updated_at: :desc).limit(100)
     end
   end
 
