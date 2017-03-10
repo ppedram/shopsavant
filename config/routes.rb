0a89040d7310caadf54d14cddb14efd988648346
ppedram <pympdr@gmail.com>
2017-03-10 14:16:10 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'collections/:collection/products/:order_by', to: 'products#index'
   get 'products/:product_handle/inventory/sku', to: 'sales#inventoryBySKU'
   get 'products/:product_handle/inventory', to: 'sales#inventoryByProduct'
-  get '/', to: 'collections#index'
+  root to: 'collections#index'
 end
