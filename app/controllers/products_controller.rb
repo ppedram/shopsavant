65ce6a62171d80620af0c60881f978dd000812c8
ppedram <pympdr@gmail.com>
2017-03-09 16:10:19 -0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
     collection = Collection.find_by(:handle => params[:collection])
     if params[:order_by] == "sales"
       @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(total_sales: :desc)
-    elsif params[:order_by] == "inventory_quantity"
+    elsif params[:order_by] == "inventory"
         @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(total_inventory: :desc)
     else
       @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(product_updated_at: :desc)
