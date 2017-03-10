3fefba228bb74188517de3ffeabe55729a34cf85
ppedram <pympdr@gmail.com>
2017-03-09 16:37:23 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'collections', to: 'collections#index'
-  get 'collections/scan', to: 'collections#scan'
-  get 'collections/:collection/products', to: 'products#index'
-  get 'collections/:collection/products/:order_by', to: 'products#index'
-  get 'products/:product_handle/inventory/sku', to: 'sales#inventoryBySKU'
-  get 'products/:product_handle/inventory', to: 'sales#inventoryByProduct'
+  get "collections", to: "collections#index"
+  get "collections/scan", to: "collections#scan"
+  get "collections/:collection/products", to: "products#index"
+  get "collections/:collection/products/:order_by", to: "products#index"
+  get "products/:product_handle/inventory/sku", to: "sales#inventoryBySKU"
+  get "products/:product_handle/inventory", to: "sales#inventoryByProduct"
 end
