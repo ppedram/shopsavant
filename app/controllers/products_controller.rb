5681a6a9874b9e94eebb28932b3c1fe854225389
ppedram <pympdr@gmail.com>
2017-03-10 17:59:05 -0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,16 +15,13 @@ class ProductsController < ApplicationController
   def scan
     Product.scan(params[:collection])
   end
-  
   def allowed_params
     params.require(:collection)
   end
-  def index
   @products = Product.order(:name)
   respond_to do |format|
     format.html
     format.csv { send_data @products.to_csv }
     format.xls # { send_data @products.to_csv(col_sep: "\t") }
   end
-end
 end
