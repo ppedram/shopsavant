cce5f2511d79a6f475c6a3fae665d8e629c41982
ppedram <pympdr@gmail.com>
2017-03-10 18:01:45 -0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,11 +17,4 @@ class ProductsController < ApplicationController
   end
   def allowed_params
     params.require(:collection)
-  end
-  @products = Product.order(:name)
-  respond_to do |format|
-    format.html
-    format.csv { send_data @products.to_csv }
-    format.xls # { send_data @products.to_csv(col_sep: "\t") }
-  end
 end
