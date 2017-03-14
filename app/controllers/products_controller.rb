0d63b050488f2d15a30b2d460aec6152fc75c6ab
ppedram <pympdr@gmail.com>
2017-03-13 18:16:48 -0700

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,18 @@ class ProductsController < ApplicationController
   def allowed_params
     params.require(:collection)
   end
+  
+    def index
+    @products = Product.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @products.to_csv(['name', 'category', 'price']) }
+    end
+  end
+
+  def import
+    Product.import(params[:file])
+    redirect_to root_url, notice: "Products imported."
+  end
+
 end
