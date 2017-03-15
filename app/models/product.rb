f93666e12ef3c99f9ce90aae03a903dc018ac315
ppedram <pympdr@gmail.com>
2017-03-15 14:48:44 -0700

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -53,7 +53,6 @@ class Product < ActiveRecord::Base
                     sku: item_v["sku"]
                 )
                 inventory = item_v["inventory_quantity"].to_i
-                if inventory > 0
                   total_inventory += inventory
                     # puts "Add Inventory: #{inventory}"
                 end
