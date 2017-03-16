367c3eaad8b243e35f1c1b8d73844bf001329856
ppedram <pympdr@gmail.com>
2017-03-16 15:23:40 -0700

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,11 +14,11 @@ class ProductsController < ApplicationController
   def index
     collection = Collection.find_by(:handle => params[:collection])
     if params[:order_by] == "sales"
-      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(total_sales: :desc)
+      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 2000).order(total_sales: :desc)
     elsif params[:order_by] == "inventory"
-        @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(total_inventory: :desc)
+        @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 2000).order(total_inventory: :desc)
     else
-      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 100).order(product_updated_at: :desc)
+      @products = Product.includes(:variants).where.not(:variants => { :id => nil }).paginate(:page => params[:page], :per_page => 2000).order(product_updated_at: :desc)
     end
   end
 
