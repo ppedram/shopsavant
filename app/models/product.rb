90338f2dac7289d3cf83d7a7d80ea1be3c449edb
ppedram <pympdr@gmail.com>
2017-03-16 15:24:24 -0700

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 require "json"
 
+def self.to_csv
+  CSV.generate do |csv|
+    csv << column_names
+    all.each do |product|
+      csv << item.attributes.values_at(*column_names)
+    end
+  end
+end
+
 class Product < ActiveRecord::Base
     validates_uniqueness_of :product_id
     belongs_to :collection
